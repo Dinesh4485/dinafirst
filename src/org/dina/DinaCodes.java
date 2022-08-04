514bfe22d43d2ebab092b7fb6be84f5822ec6423
Dinesh4485 <aruldinesh1995gmail.com>
2022-08-04 14:45:10 +0530

    Project Structure

---
+++ src/org/dina/DinaCodes.java
@@ -0,0 +1,13 @@
+package org.dina;
+
+
+public class DinaCodes {
+private void name() {
+int dinaage =25;
+System.out.println(dinaage);
+}
+public static void main(String[] args) {
+	DinaCodes dina = new DinaCodes();
+	dina.name();
+}
+}
